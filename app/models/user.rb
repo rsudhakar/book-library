dcfb25395e8c5f3a5d48ffd4279ad967edf143e1
vinoth <vinoth@tarkalabs.com>
2017-08-02 11:50:52 +0530

    Add user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :books
+  LENDING_CAP = 2
+
+  def can_lend?
+    books.count < LENDING_CAP
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { create :user }
+
+  describe 'can_lend?' do
+    context 'user has borrowed less than quota' do
+      it 'should return true' do
+        create(:book, user: user)
+        expect(user.can_lend?).to be_truthy
+      end
+    end
+
+    context 'user has borrowed upto his quota' do
+      it 'should return false' do
+        create_list(:book, 2, user: user)
+        expect(user.can_lend?).to be_falsey
+      end
+    end
+  end
 end
