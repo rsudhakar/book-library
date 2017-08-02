1a21b332fce7c0b9c5605c91088e155610a69d0e
vinoth <vinoth@tarkalabs.com>
2017-08-02 12:17:05 +0530

    Add book spec and book lending functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :book_info
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  def lend(lending_user)
+    return if (user.present? || !lending_user.can_lend?)
+
+    update(user: lending_user)
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:lent_book) { create :book }
+  let(:available_book) { create :book, user: nil}
+  let(:user) { create :user }
+
+  describe 'lend' do
+    context 'book is already lent' do
+      it 'should not lend the book' do
+        lent_book.lend(user)
+        expect(lent_book.user_id).to_not eq(user.id)
+      end
+    end
+
+    context 'book is available' do
+      context 'user has quota' do
+        it 'should lend the book' do
+          available_book.lend(user)
+          available_book.reload
+          expect(available_book.user_id).to eq(user.id)
+        end
+      end
+
+      context 'user does not have quota' do 
+        it 'should not lend the book' do
+          create_list(:book, 2, user: user)
+          available_book.lend(user)
+          available_book.reload
+          expect(available_book.user_id).to_not eq(user.id)
+        end
+      end
+    end
+  end
 end
